e754c372ca94881de156eb9b6f03f1cbfe200d7a
David Millman <dave@cs.unc.edu>
2018-10-25 17:28:50 -0600

    Restructure data

--- data/COMPANY/load-data.sql
+++ data/COMPANY/load-data.sql
@@ -6,12 +6,12 @@ drop table if exists Project;
 drop table if exists Works_on;
 
 .mode csv
-.import COMPANY/DEPARTMENT.csv     Department
-.import COMPANY/DEPENDENT.csv      Dependent
-.import COMPANY/DEPT_LOCATION.csv  Dept_location
-.import COMPANY/EMPLOYEE.csv       Employee
-.import COMPANY/PROJECT.csv        Project
-.import COMPANY/WORKS_ON.csv       Works_on
+.import DEPARTMENT.csv     Department
+.import DEPENDENT.csv      Dependent
+.import DEPT_LOCATION.csv  Dept_location
+.import EMPLOYEE.csv       Employee
+.import PROJECT.csv        Project
+.import WORKS_ON.csv       Works_on
 
 update Employee set Super_ssn = null where Super_ssn = 'NULL';
 update Works_on set Hours = null where Hours = 'NULL';
