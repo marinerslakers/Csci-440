2ec0d96d5934c7fce7a21f898660787184878b70
Brandon May <brandon.may1026@gmail.com>
2018-11-01 16:29:05 -0600

    Forever Alone Query

--- projects/part03/query12.sql
+++ projects/part03/query12.sql
@@ -1,1 +1,6 @@
 .read data.sql
+.headers on
+.mode column
+
+select name, grade from Highschooler where ID not in (select distinct ID1 from Likes Union select ID2 from Likes) 
+order by grade, name;
