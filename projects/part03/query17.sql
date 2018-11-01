4ab4d2403bf342392a83bfc6cd94ed2e4fcc6bbc
Jase Rost <jaserost@yahoo.com>
2018-11-01 16:32:20 -0600

    Finished love triangle query

--- projects/part03/query17.sql
+++ projects/part03/query17.sql
@@ -1,1 +1,6 @@
 .read data.sql
+
+select D.ID, D.name, D.grade from ((select A.ID as ID, A.name as name, A.grade as grade, Count(*) as num from Highschooler A, Likes B where
+		A.ID = B.ID2
+		group by A.ID) D)
+	where D.num > 1;
