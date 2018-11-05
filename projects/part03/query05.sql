40196ca414327f26680882e7a1e1ac257d8d23f7
Jase Rost <jaserost@yahoo.com>
2018-11-05 12:03:24 -0700

    Finished query05, fixed query14

--- projects/part03/query05.sql
+++ projects/part03/query05.sql
@@ -1,1 +1,6 @@
 .read data.sql
+
+select A.name, A.grade from Highschooler A where
+	A.ID not in (select B.ID from Highschooler B, Highschooler C, Friend D where
+		((B.ID = D.ID1 and C.ID = D.ID2) or (B.ID = D.ID2 and C.ID = D.ID1)) and
+		B.grade = C.grade);
--- projects/part03/query14.sql
+++ projects/part03/query14.sql
@@ -1,7 +1,7 @@
 .read data.sql
 
-select distinct A.ID, A.name, A.grade from Highschooler A where
+select distinct A.name, A.grade from Highschooler A where
 	A.ID not in (select B.ID from Highschooler B, Highschooler C, Friend D where
-		((B.ID = D.ID1 and C.ID = D.ID2) or (B.ID = D.ID2 and C.ID = D.ID2)) and
+		((B.ID = D.ID1 and C.ID = D.ID2) or (B.ID = D.ID2 and C.ID = D.ID1)) and
 		B.grade != C.grade)
 		order by A.grade desc, A.name;	
