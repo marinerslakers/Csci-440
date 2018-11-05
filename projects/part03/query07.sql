82772a8a07c64ac4cf92ddaa0bb11fb8d5f49941
Jase Rost <jaserost@yahoo.com>
2018-11-05 12:36:13 -0700

    Finished query07

--- projects/part03/query07.sql
+++ projects/part03/query07.sql
@@ -1,1 +1,17 @@
 .read data.sql
+
+--select * from Highschooler A where
+--	A.ID in (select * from (select B.ID from Highschooler B, Friend C where
+--			(B.ID = C.ID1 and 1709 = C.ID2)
+--			or (B.ID = C.ID2 and 1709 = C.ID1)) D where
+--		D.ID in (select E.ID from Highschooler F, Friend G where
+--	and A.ID != 1709;
+	
+	
+select COUNT(*) from Highschooler A where
+	A.ID in (select E.ID from (select * from (select B.ID from Highschooler B, Friend C where
+			(B.ID = C.ID1 and 1709 = C.ID2)
+			or (B.ID = C.ID2 and 1709 = C.ID1))) D, Highschooler E, Friend F where
+		(D.ID = F.ID1 and E.ID = F.ID2) or (D.ID = F.ID2 and E.ID =  F.ID1) or
+		E.ID = D.ID)
+	and A.ID != 1709;
