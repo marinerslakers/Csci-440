57b04d2fc7f47a3c74a29141df0903a05d3a7a2f
Jase Rost <jaserost@yahoo.com>
2018-11-05 12:39:17 -0700

    Removed unnecessary comments for query07

--- projects/part03/query07.sql
+++ projects/part03/query07.sql
@@ -1,12 +1,4 @@
 .read data.sql
-
---select * from Highschooler A where
---	A.ID in (select * from (select B.ID from Highschooler B, Friend C where
---			(B.ID = C.ID1 and 1709 = C.ID2)
---			or (B.ID = C.ID2 and 1709 = C.ID1)) D where
---		D.ID in (select E.ID from Highschooler F, Friend G where
---	and A.ID != 1709;
-	
 	
 select COUNT(*) from Highschooler A where
 	A.ID in (select E.ID from (select * from (select B.ID from Highschooler B, Friend C where
