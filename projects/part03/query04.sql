1dd686967ccdf7085e4700f4213b95b12b6f70ad
Zak Alberda <zkiam.a3992@gmail.com>
2018-11-05 15:11:25 -0700

    Query 3,4 updated

--- projects/part03/query04.sql
+++ projects/part03/query04.sql
@@ -1,1 +1,8 @@
 .read data.sql
+
+select HighSchooler_1.name, HighSchooler_1.grade, HighSchooler_2.name, HighSchooler_2.grade, HighSchooler_3.name, HighSchooler_3.grade
+from Likes L1, Likes L2, Highschooler HighSchooler_1, Highschooler HighSchooler_2, Highschooler HighSchooler_3
+where L1.ID2 = L2.ID1
+and L2.ID2 <> L1.ID1
+and L1.ID1 = HighSchooler_1.ID and L1.ID2 = HighSchooler_2.ID and L2.ID2 = HighSchooler_3.ID
+;
