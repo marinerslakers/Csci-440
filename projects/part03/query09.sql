a0ff7856f8ed8155cb5c3ceeccb818791dfe4db3
Jase Rost <jaserost@yahoo.com>
2018-11-05 14:49:53 -0700

    Finished query09

--- projects/part03/query09.sql
+++ projects/part03/query09.sql
@@ -1,1 +1,7 @@
 .read data.sql
+
+select A.name from Highschooler A, Friend B where
+	(A.ID = B.ID1 and 
+	(B.ID2 in (select ID from Highschooler where name = "Gabriel")))
+	or (A.ID = B.ID2 and
+	(B.ID1 in (select ID from Highschooler where name = "Gabriel")));
