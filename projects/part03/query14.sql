646fd8de2d3a85c7a246a55b6dcb394bac1ebe15
Jase Rost <jaserost@yahoo.com>
2018-11-05 11:38:15 -0700

    Finished query 14

--- projects/part03/query14.sql
+++ projects/part03/query14.sql
@@ -1,1 +1,7 @@
 .read data.sql
+
+select distinct A.ID, A.name, A.grade from Highschooler A where
+	A.ID not in (select B.ID from Highschooler B, Highschooler C, Friend D where
+		((B.ID = D.ID1 and C.ID = D.ID2) or (B.ID = D.ID2 and C.ID = D.ID2)) and
+		B.grade != C.grade)
+		order by A.grade desc, A.name;	
