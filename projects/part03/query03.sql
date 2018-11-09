ad206cbe7b7fc5b26811fbb7c5c27c6a6ca132de
Jase Rost <jaserost@yahoo.com>
2018-11-09 11:34:06 -0700

    Commit to differentiate

--- projects/part03/query03.sql
+++ projects/part03/query03.sql
@@ -1,6 +1,8 @@
 .read data.sql
 
-insert into friend select friend1.id1 as friendA, friend2.id2 as friendB from friend friend1 join friend friend2 on friend1.id2 = friend2.id1 where friend1.id1 <> friend2.id2 except
-select * from friend;
+insert into friend select friend1.id1 as friendA, friend2.id2 as friendB
+	from friend friend1 join friend friend2 on friend1.id2 = friend2.id1
+	where friend1.id1 <> friend2.id2 except
+	select * from friend;
 
 select * from friend;
